ce66df84f4ffd81ce8b9b68a7817182b4316c644
Xavster838 <francisguitart@gmail.com>
2021-06-23 11:37:58 -0700

    Specify Prototype data, first call to generate recan object. Need MSA prototype data instead of current.

--- learning_recan/prototype_simgen_CYP2D6.py
+++ learning_recan/prototype_simgen_CYP2D6.py
@@ -1,1 +1,10 @@
 #June 23, 2021: Trying out recan simgen to generate window aligner plots for my CYP2D duplicons
+from recan.simgen import Simgen
+import os
+
+#specify prototype data
+HOME_DIR="os.getcwd()"
+#input data needs to be an MSA
+proto_fa="prototype_data/sample_CYP2D6_duplicons.fa" 
+
+sim_obj = Simgen(proto_fa)
